e7a696c294efdf909dd03ffe9638093f0352cbdc
Hitesh Rathod IW0282 <hitesh.rathod@identixweb.in>
2025-11-19 15:35:34 +0530

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const http = require('http');
 
 // const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Create a server that responds with "Hello, World!"
 const server = http.createServer((req, res) => {
@@ -14,12 +15,12 @@ const server = http.createServer((req, res) => {
 
     res.statusCode = 200; // HTTP status code for OK
     res.setHeader('Content-Type', 'text/plain'); // Set response header
-    res.end('Hello, World! iam 33444 \n'); // Send the response
+    res.end('Hello, World! iam 334445555 \n'); // Send the response
 });
 
 
 
 // Listen on port 3000
 server.listen(PORT, () => {
-    console.log('Server running 33334444 : ', PORT);
+    console.log(`Server running 333444555  on port: ${PORT}`);
 });
